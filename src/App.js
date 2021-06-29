3b89e49c16d465cf915505814dc71f634cd899de
Dulgiduck <jsh@sngy.io>
2021-06-29 13:41:40 +0900

    ENH: Building the Navigation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import {HashRouter, Route} from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
 function App(){
-  return <HashRouter>
+  return (
+  <HashRouter>
+    <Navigation></Navigation>
     <Route path="/" exact={true} component={Home}></Route>
     <Route path="/about" component={About}></Route>
-  </HashRouter>
+  </HashRouter> );
 }
 
 export default App;
