50f1437147f7e23577b747c29fde8133ada94834
Dulgiduck <jsh@sngy.io>
2021-06-29 14:20:32 +0900

    ENH: redirecting

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import "./Navigation.css";
 
 function Navigation(){
     return (
-    <div>
+    <div className="nav">
         <Link to="/">Home</Link>
         <Link to={{pathname: "/about", state: {fromNabvigation: true}}}>About</Link>
     </div>);
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-function Detail(props){
-    return <h1>hello</h1>;
+class Detail extends React.Component{
+    componentDidMount(){
+        console.log(this.props);
+        const {location, history} = this.props;
+        if(location.state === undefined){
+            history.push("/");
+        }
+    }
+    render(){
+        const {location}= this.props;
+        if(location.state){
+            return <span> {location.state.title} </span>;
+        }else{
+            return null;
+        }
+    }
 }
 
 export default Detail;
